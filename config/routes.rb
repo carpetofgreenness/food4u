b22f6aab33690a4a612b54e63bbbda71a560345e
An Ngo <ngoan_230@yahoo.com>
2017-03-10 16:25:58 -0500

    relationships and kitchen controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :food, :kitchen, :list
+  resources :foods, :lists, :kitchens, only: [:create, :update, :destroy] 
   root "pages#home"
   
   get "/list", to: "pages#list"
