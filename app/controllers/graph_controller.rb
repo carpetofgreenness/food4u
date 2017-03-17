ef978764220a62b8a3f9b5a53e6be696a356a256
Hannah Weinstein <hannahjweinstein@gmail.com>
2017-03-17 17:33:18 -0400

    populate graph controller

--- app/controllers/graph_controller.rb
+++ app/controllers/graph_controller.rb
@@ -1,2 +1,15 @@
 class GraphController < ApplicationController
+
+  def index
+  end
+ 
+  def data
+    respond_to do |format|
+      format.json {
+        render :json => Graveyard.make_table
+      }
+    end
+  end
+
+
 end
