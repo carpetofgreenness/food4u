1790011d622975281e676c549a7326279c3dd166
Hannah Weinstein <hannahjweinstein@gmail.com>
2017-03-19 16:20:29 -0400

    looks a little nicer, but why does it show the times during the day? also, it is showing everyone's graveyard at the moment, need to change to current user

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,77 +18,83 @@
 //= require_tree .
 
 $("document").ready(function(){
-	console.log("hi")
 	$.ajax({
-				url: "../graph/data", // in pages_controller.rb
-				type: 'GET',
-				// data: { food: "apple" },
-				dataType: "json",
-	           success: function (data) {
-	               draw(data["results"]);
-	               // console.log(data)
-	           },
-	           error: function (result) {
-	               console.log("here was an error")
-	               error();
-	           }
-	       });
+		url: "../graph/data", // in graph_controller.rb
+		type: 'GET',
+		dataType: "json",
+		success: function (data) {
+		   draw(data["results"]);
+		},
+		error: function (result) {
+		   console.log("here was an error")
+		   error();
+		}
+	});
 
-function draw(data) {
+	function draw(data) {
 
 	console.log(data)
 
-// set the dimensions and margins of the graph
-var margin = {top: 20, right: 20, bottom: 30, left: 50},
-    width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+	// set the dimensions and margins of the graph
+	// uses actual width and height of screen
+	var margin = {top: 20, right: 20, bottom: 30, left: 50};
+		// if the screen is large, make it half the screen
+		// otherwise make it the whole screen
+		if ($(window).width() > 580) {
+	    	var width = $( window ).width()/2 - margin.left - margin.right;
+	    	console.log($(window).width())
+		}
+		else {
+			var width = $( window ).width() - margin.left - margin.right;
+		}
+	    height = $( window ).height()/2 - margin.top - margin.bottom;
 
-// parse the date / time
-var parseTime = d3.timeParse("%Y-%m-%d");
+	// create function to parse the date / time
+	var parseTime = d3.timeParse("%Y-%m-%d");
 
-// set the ranges
-var x = d3.scaleTime().range([0, width]);
-var y = d3.scaleLinear().range([height, 0]);
+	// set the ranges
+	var x = d3.scaleTime().range([0, width]);
+	var y = d3.scaleLinear().range([height, 0]);
 
-// define the line
-var valueline = d3.line()
-    .x(function(d) { return x(d[0]); })
-    .y(function(d) { return y(d[1]); });
+	// define the line
+	var valueline = d3.line()
+	    .x(function(d) { return x(d[0]); })
+	    .y(function(d) { return y(d[1]); });
 
-// append the svg obgect to the body of the page
-// appends a 'group' element to 'svg'
-// moves the 'group' element to the top left margin
-var svg = d3.select("body").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform",
-          "translate(" + margin.left + "," + margin.top + ")");
+	// append the svg obgect to the body of the page
+	// appends a 'group' element to 'svg'
+	// moves the 'group' element to the top left margin
+	var svg = d3.select("body").append("svg")
+	    .attr("width", width + margin.left + margin.right)
+	    .attr("height", height + margin.top + margin.bottom)
+	  	.append("g")
+	    .attr("transform",
+	          "translate(" + margin.left + "," + margin.top + ")");
 
-  // format the data
-  data.forEach(function(d) {
-      d[0] = parseTime(d[0]);
-      d[1] = +d[1];
-  });
+	  // format the data
+	  data.forEach(function(d) {
+	      d[0] = parseTime(d[0]);
+	      d[1] = +d[1];
+	  });
 
-  // Scale the range of the data
-  x.domain(d3.extent(data, function(d) { return d[0]; }));
-  y.domain([0, d3.max(data, function(d) { return d[1]; })]);
+	  // Scale the range of the data
+	  x.domain(d3.extent(data, function(d) { return d[0]; }));
+	  y.domain([0, 100]);
 
-  // Add the valueline path.
-  svg.append("path")
-      .data([data])
-      .attr("class", "line")
-      .attr("d", valueline);
+	  // Add the valueline path.
+	  svg.append("path")
+	      .data([data])
+	      .attr("class", "line")
+	      .attr("d", valueline);
 
-  // Add the X Axis
-  svg.append("g")
-      .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(x));
+	  // Add the X Axis
+	  svg.append("g")
+	      .attr("transform", "translate(0," + height + ")")
+	      .call(d3.axisBottom(x));
 
-  // Add the Y Axis
-  svg.append("g")
-      .call(d3.axisLeft(y));
+	  // Add the Y Axis
+	  svg.append("g")
+	      .call(d3.axisLeft(y));
 
 
 }
